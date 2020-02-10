743d3d65611d45d492b27b8522c405e8e45c0aac
hangaih <hangai.acc@gmail.com>
2020-02-10 14:26:58 +0900

    変数の参照・代入機能の追加

--- interp.rb
+++ interp.rb
@@ -1,27 +1,30 @@
 require "minruby"
-
-def evaluate(tree)
+def evaluate(tree, env)
   case tree[0]
   when "lit"
     tree[1]
   when "+"
-    evaluate(tree[1]) + evaluate(tree[2])
+    evaluate(tree[1], env) + evaluate(tree[2], env)
   when "-"
-    evaluate(tree[1]) + evaluate(tree[2])
+    evaluate(tree[1], env) - evaluate(tree[2], env)
   when "*"
-    evaluate(tree[1]) * evaluate(tree[2])
+    evaluate(tree[1], env) * evaluate(tree[2], env)
   when "/"
-    evaluate(tree[1]) / evaluate(tree[2])
+    evaluate(tree[1], env) / evaluate(tree[2], env)
   when "func_call" #仮の実装
-    p(evaluate(tree[2]))
+    p(evaluate(tree[2], env))
   when "stmts"
     i = 1
     last = nil
-    while tree[i] != nil
-      last = evaluate(tree[i])
+    while tree[i]
+      last = evaluate(tree[i], env)
       i = i + 1
     end
     last
+  when "var_assign"
+    env[tree[1]] = evaluate(tree[2], env)
+  when "var_ref"
+    env[tree[1]]
   end
 end
   
@@ -29,7 +32,9 @@ end
 str = minruby_load()
 # ②計算式の文字列を計算の木に変換する
 tree = minruby_parse(str)
-pp(tree)
+pp(tree) #構文解析された文字列を確認
 # ③計算の木を実行（計算）する
-answer = evaluate(tree)
+env = {}
+evaluate(tree, env)
+
 

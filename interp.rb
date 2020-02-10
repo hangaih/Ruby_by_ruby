7a6c8ed6b776059bdf8a5a1c2f5da95df76bfb23
hangaih <hangai.acc@gmail.com>
2020-02-10 13:51:56 +0900

    複文の対応

--- interp.rb
+++ interp.rb
@@ -14,6 +14,14 @@ def evaluate(tree)
     evaluate(tree[1]) / evaluate(tree[2])
   when "func_call" #仮の実装
     p(evaluate(tree[2]))
+  when "stmts"
+    i = 1
+    last = nil
+    while tree[i] != nil
+      last = evaluate(tree[i])
+      i = i + 1
+    end
+    last
   end
 end
   
@@ -21,6 +29,7 @@ end
 str = minruby_load()
 # ②計算式の文字列を計算の木に変換する
 tree = minruby_parse(str)
+pp(tree)
 # ③計算の木を実行（計算）する
 answer = evaluate(tree)
 
